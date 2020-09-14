08abb8229cabc26506fa68af37e8256a54d4183d
Paul Onteri <onteripaul@gmail.com>
2020-09-15 00:06:01 +0300

    allowed hosts

--- django_azure/settings.py
+++ django_azure/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@j%8$5lg#=6qyxxu*zb5i5dzehscu()&@kss8bu19q#m90*)&b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
